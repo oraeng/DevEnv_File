57baf628ac55bda732478caeeae4d5e6ead843f2
oraeng <38767562+oraeng@users.noreply.github.com>
2021-01-07 01:56:45 +0900

    Update ubuntu_sim_ros_melodic.sh

--- ubuntu_sim_ros_melodic.sh
+++ ubuntu_sim_ros_melodic.sh
@@ -8,43 +8,6 @@
 ## - ROS Melodic (including Gazebo9)
 ## - MAVROS
 
-if [[ $(lsb_release -sc) == *"xenial"* ]]; then
-  echo "OS version detected as $(lsb_release -sc) (16.04)."
-  echo "ROS Melodic requires at least Ubuntu 18.04."
-  echo "Exiting ...."
-  return 1;
-fi
-
-echo "Downloading dependent script 'ubuntu_sim_common_deps.sh'"
-# Source the ubuntu_sim_common_deps.sh script directly from github
-common_deps=$(wget https://raw.githubusercontent.com/PX4/Devguide/master/build_scripts/ubuntu_sim_common_deps.sh -O -)
-wget_return_code=$?
-# If there was an error downloading the dependent script, we must warn the user and exit at this point.
-if [[ $wget_return_code -ne 0 ]]; then echo "Error downloading 'ubuntu_sim_common_deps.sh'. Sorry but I cannot proceed further :("; exit 1; fi
-# Otherwise source the downloaded script.
-. <(echo "${common_deps}")
-
-# ROS Melodic
-## Gazebo simulator dependencies
-sudo apt-get install protobuf-compiler libeigen3-dev libopencv-dev -y
-
-## ROS Gazebo: http://wiki.ros.org/melodic/Installation/Ubuntu
-## Setup keys
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-## For keyserver connection problems substitute hkp://pgp.mit.edu:80 or hkp://keyserver.ubuntu.com:80 above.
-sudo apt-get update
-## Get ROS/Gazebo
-sudo apt install ros-melodic-desktop-full -y
-## Initialize rosdep
-sudo rosdep init
-rosdep update
-## Setup environment variables
-rossource="source /opt/ros/melodic/setup.bash"
-if grep -Fxq "$rossource" ~/.bashrc; then echo ROS setup.bash already in .bashrc;
-else echo "$rossource" >> ~/.bashrc; fi
-eval $rossource
-
 ## Install rosinstall and other dependencies
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
 
