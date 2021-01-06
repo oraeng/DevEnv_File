46bdd33dff17eca2ff1c166b61f38f6a0085d712
oraeng <38767562+oraeng@users.noreply.github.com>
2021-01-07 01:13:06 +0900

    Update ubuntu_sim_ros_melodic.sh

--- ubuntu_sim_ros_melodic.sh
+++ ubuntu_sim_ros_melodic.sh
@@ -55,8 +55,8 @@ sudo apt install python-rosdep python-rosinstall python-rosinstall-generator pyt
 sudo apt-get install python-catkin-tools python-rosinstall-generator -y
 
 ## Create catkin workspace
-mkdir -p /root/catkin_ws/src
-cd /root/catkin_ws
+mkdir -p /root/ros1_ws/src
+cd /root/ros1_ws
 catkin init
 wstool init src
 
