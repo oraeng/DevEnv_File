c1cad0f64cffd9f1fa40be778c0cba7238dabdad
oraeng <38767562+oraeng@users.noreply.github.com>
2021-01-16 04:23:47 +0900

    Update PX4_Install.sh

--- PX4_Install.sh
+++ PX4_Install.sh
@@ -5,11 +5,11 @@ git clone https://github.com/PX4/px4_msgs.git ~/robot_ws/src/px4_msgs
 cd ~/catkin_ws/src
 git clone https://github.com/PX4/px4_ros_com.git ~/catkin_ws/src/px4_ros_com -b ros1
 git clone https://github.com/PX4/px4_msgs.git ~/catkin_ws/src/px4_msgs -b ros1
-source /opt/ros/foxy/setup.bash
-git clone https://github.com/ros2/ros1_bridge.git -b foxy ~/robot_ws/src/ros1_bridge
+source /opt/ros/dashing/setup.bash
+git clone https://github.com/ros2/ros1_bridge.git -b dashing ~/robot_ws/src/ros1_bridge
 cd /root/robot_ws/
 colcon build --symlink-install --packages-skip ros1_bridge --event-handlers console_direct+
-source /opt/ros/noetic/setup.bash
+source /opt/ros/melodic/setup.bash
 cd /root/catkin_ws
 colcon build --symlink-install --event-handlers console_direct+
 source ~/catkin_ws/install/setup.bash
