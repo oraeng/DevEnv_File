83088c052b2012cb779e41c567a39721046b51c3
oraeng <38767562+oraeng@users.noreply.github.com>
2021-01-16 00:41:31 +0900

    Update and rename ubuntu_sim_ros_melodic.sh to PX4_Install.sh

---
+++ PX4_Install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source ~/.bashrc
+git clone https://github.com/PX4/px4_ros_com.git ~/robot_ws/src/px4_ros_com
+git clone https://github.com/PX4/px4_msgs.git ~/robot_ws/src/px4_msgs
+cd ~/catkin_ws/src
+git clone https://github.com/PX4/px4_ros_com.git ~/catkin_ws/src/px4_ros_com -b ros1
+git clone https://github.com/PX4/px4_msgs.git ~/catkin_ws/src/px4_msgs -b ros1
+source /opt/ros/foxy/setup.bash
+git clone https://github.com/ros2/ros1_bridge.git -b foxy ~/robot_ws/src/ros1_bridge
+cd /root/robot_ws/
+colcon build --symlink-install --packages-skip ros1_bridge --event-handlers console_direct+
+source /opt/ros/noetic/setup.bash
+cd /root/catkin_ws
+colcon build --symlink-install --event-handlers console_direct+
+source ~/catkin_ws/install/setup.bash
+/bin/bash -c source ~/robot_ws/install/setup.bash
+cd /root/robot_ws
+colcon build --symlink-install --packages-select ros1_bridge --cmake-force-configure --event-handlers console_direct+
